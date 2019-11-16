3bf8f95527506f7ed60c8e272f18f417f83ff538
strazds <strazds@gmail.com>
2019-11-16 22:48:52 +0100

    update 201911162248

--- react/001_course/src/App.js
+++ react/001_course/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
 import "./App.css";
-import Lesson from "./001_module/003_lesson.js";
+import Lesson from "./001_module/001_lesson.js";
 
 export default () => <Lesson />;
